b7f7753bc60082562604228f740473d2b62e9bf2
manchking <manchking@yandex.ru>
2020-03-21 14:27:34 +0400

    wip: transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw Error();
+    if(arr.length === 0) return arr;
+    let res = [];
+    let action;
+    arr.map((item, index) => {
+        switch(item){
+            case '--discard-next':
+                break;
+            case '--discard-prev':
+                if(res[index-1]) res.splice(1, index-1);
+                break;
+            case '--double-next':
+                if(arr[index+1]) res.push(arr[index+1]);
+                break;
+            case '--double-prev':
+                if(res[index-1]) res.push(arr[index-1]);
+                break;
+            default: if(arr[index-1] === '--discard-next'){
+                break;
+            }else{
+                res.push(item);
+            }
+        }
+    })
+    return res;
+}
